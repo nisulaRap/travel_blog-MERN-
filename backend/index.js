b264d271f55491827545f1dd8cfff1d2508976bf
Nisula Premaratne <nisula.rap@gmail.com>
2024-12-03 21:04:38 +0530

    tested api

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const bcrypt = require("bcrypt");
+const express = require("express");
+const cors = require("cors");
+
+const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// Test api
+app.get("/hello", async (req,res) => {
+    return res.status(200).json({ message: "hello" });
+});
+
+app.listen(8000);
+module.exports = app;

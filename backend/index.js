d14a4bca44c69a190345a3865c9718a99397bfa2
Nisula Premaratne <nisula.rap@gmail.com>
2024-12-03 23:35:10 +0530

    created database url

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
 
 const jwt = require("jsonwebtoken");
 
+mongoose.connect(config.connectionString);
+
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-// Test api
-app.get("/hello", async (req,res) => {
-    return res.status(200).json({ message: "hello" });
+// Create Account
+app.get("/create-account", async (req,res) => {
+    
 });
 
 app.listen(8000);

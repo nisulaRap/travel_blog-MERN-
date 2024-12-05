e92942098807e4e03a52a0d36fe81ce1a5542a90
Nisula Premaratne <nisula.rap@gmail.com>
2024-12-05 11:17:20 +0530

    created user in index.js file

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,9 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
-
 const jwt = require("jsonwebtoken");
+
+const User = require("./models/user_model");
 
 mongoose.connect(config.connectionString);
 
@@ -16,7 +17,16 @@ app.use(cors({ origin: "*" }));
 
 // Create Account
 app.get("/create-account", async (req,res) => {
-    
+    const { fullName, email, password } = req.body;
+
+    if (!fullName || !email || !password) {
+        return res.status(400).json({ error: true, message: "All fields are required" });
+    }
+
+    const isUser = await User.findOne({ email });
+    if (isUser) {
+        return res.status(400).json({ error: true, message: "User already exists" });
+    }
 });
 
 app.listen(8000);

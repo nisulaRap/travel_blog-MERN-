0d5a02c1c399e879c4d673297836b90e7a24b6f3
Nisula Premaratne <nisula.rap@gmail.com>
2024-12-16 12:03:28 +0530

    created search in index,js file

--- backend/index.js
+++ backend/index.js
@@ -297,5 +297,30 @@ app.put("/update-is-favourite/:id", authenticateToken, async (req,res) => {
     }
 });
 
+// Search Travel Stories
+app.get("/search", authenticateToken, async (req,res) => {
+    const { query } = req.query;
+    const { userId } = req.user;
+
+    if(!query){
+        return res.status(404).json({ error: true, message: "query is required" });
+    }
+
+    try{
+        const searchResults = await TravelBlog.find({
+            userId: userId,
+            $or: [
+                { title: { $regex: query, $options: "i" }},
+                { story: { $regex: query, $options: "i" }},
+                { visitedLocation: { $regex: query, $options: "i" }},
+            ],
+        }).sort({ isFavourite: -1 });
+
+        res.status(200).json({stories: searchResults});
+    }catch(error){
+        res.status(500).json({ error: true, message: error.message });
+    }
+});
+
 app.listen(8000);
 module.exports = app;

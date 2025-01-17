84fd6182f03482ddfc0abe4db9ee0e9d3f8a783f
nisulaRap <nisula.rap@gmail.com>
2025-01-17 14:49:41 +0530

    added validation for email and password

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 import PasswordInput from "../../components/input/PasswordInput";
+import { useNavigate } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (password) {
+      setError("Please enter a password.");
+      return;
+    }
+
+    setError("");
+
+    // Call the login API here
+  };
+
   return (
     <div className="h-screen bg-cyan-100 overflow-hidden relative">
+
+      <div className="login-ui-box right-10 -top-40" />
+      <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2" />
+
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
         <div className="w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
           <div>
@@ -18,12 +49,14 @@ const Login = () => {
         </div>
 
         <div className="w-1/3 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2x1 font-semibold mb-7">Login</h4>
 
-            <input type="text" placeholder="Email" className="input-box" />
+            <input type="text" placeholder="Email" className="input-box" value={email} onChange={({ target }) => { setEmail(target.value); }} />
 
-            <PasswordInput />
+            <PasswordInput value={password} onChange={({ target }) => { setPassword(target.value); }} />
+
+            {error && <p className="text-xs text-red-500 pb-1">{error}</p>}
 
             <button type="submit" className="btn-primary">
               LOGIN

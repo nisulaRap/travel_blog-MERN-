40b5f8f48be52af86ad4b5224bbfcd963f40064c
nisulaRap <nisula.rap@gmail.com>
2025-01-19 16:16:33 +0530

    added travel blog card with isfavourite

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
+import TravelStoryCard from "../../components/Cards/TravelStoryCard";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
   const [userInfo, setUserInfo] = useState(null);
+  const [allStories, setAllStories] = useState([]);
 
   // Get User Info
   const getUserInfo = async () => {
@@ -27,7 +31,46 @@ const Home = () => {
     }
   };
 
+  // Get all travel blogs
+  const getAllTravelStories = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-blogs");
+      if (response.data && response.data.stories) {
+        // Set all travel stories if data exists
+        setAllStories(response.data.stories);
+      }
+    }
+    catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
+    }
+  };
+
+  //Handle Edit Story Click
+  const handleEdit = (data) =>{}
+
+  // Handle Travel Story Click
+  const handleViewStory = (data) => {}
+
+  // Handle Update Favourite
+  const updateIsFavourite = async (storyData) => {
+    const storyId = storyData._id;
+
+    try{
+      const response = await axiosInstance.put("/update-is-favourite/" + storyId,
+        { isFavourite: !storyData.isFavourite, }
+      );
+
+      if (response.data && response.data.story) {
+        toast.success("Story Updated Successfully");
+        getAllTravelStories();
+      }
+    }catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
+    }
+  };
+
   useEffect(() => {
+    getAllTravelStories();
     getUserInfo();
   
     return () => {}
@@ -38,7 +81,38 @@ const Home = () => {
     <>
       <Navbar userInfo={userInfo} />
 
-      <div className="container mx-auto py-10"></div>
+      <div className="container mx-auto py-10">
+        <div className="flex gap-7">
+          <div className="flex-1">
+            {allStories.length > 0 ? (
+              <div className="grid grid-cols-2 gap-4">
+                {allStories.map((item) => {
+                  return (
+                    <TravelStoryCard 
+                      key={item._id} 
+                      imgUrl={item.imageUrl}
+                      title={item.title}
+                      story={item.story}
+                      date={item.visitedDate}
+                      visitedLocation={item.visitedLocation}
+                      isFavourite={item.isFavourite}
+                      onEdit={() => handleEdit(item)}
+                      onClick={() => handleViewStory(item)}
+                      onFavouriteClick={() => updateIsFavourite(item)}
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <>Empty Card here</>
+            )}
+          </div>
+
+          <div className="w-[320px]"></div>
+        </div>
+      </div>
+
+      <ToastContainer />
     </>
   )
 }

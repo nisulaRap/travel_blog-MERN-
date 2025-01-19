980d907f581e4dfa1d5df642fbf2639aba6e7551
nisulaRap <nisula.rap@gmail.com>
2025-01-19 20:34:13 +0530

    added add and close button in add and edit travel page

--- frontend/src/pages/Home/AddEditTravelStory.jsx
+++ frontend/src/pages/Home/AddEditTravelStory.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdAdd, MdDeleteOutline, MdUpdate, MdClose } from "react-icons/md";
 
 const AddEditTravelStory = ({
     storyInfo,
@@ -7,7 +8,25 @@ const AddEditTravelStory = ({
     getAllTravelStories,
 }) => {
   return (
-    <div>AddEditTravelStory</div>
+    <div>
+      <div className="flex items-center justify-between">
+        <h5 className="text-xl font-medium text-slate-700">
+          {type === "add" ? "Add Story" : "Update Story"}
+        </h5>
+
+        <div>
+          <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
+            <button className="btn-small" onClick={() => {}}>
+              <MdAdd className="text-lg" /> ADD STORY
+            </button>
+
+            <button className="" onClick={onClose}>
+              <MdClose className="text-xl text-slate-400" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
